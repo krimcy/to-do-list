35e2581e937b81cc80b6dd8f41d77b39a2dff0fe
Krimcy behrani <77076454+krimcy@users.noreply.github.com>
2026-06-06 01:17:21 +0200

    model updated

--- model.py
+++ model.py
@@ -1,17 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
+
+
 from flask_login import UserMixin
 
+
 db = SQLAlchemy()
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(300), nullable=False)
     completed = db.Column(db.Boolean, default=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    print ("Todo model created successfully")
