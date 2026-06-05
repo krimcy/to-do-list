0025dce155bff4a4d7bacf40a7843e574a30cd90
Tanish bhardwaj <151875069+Stentwebber@users.noreply.github.com>
2026-06-06 01:09:06 +0200

    fixing venv

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from models import db, User, Todo
+from model import db, User, Todo
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
