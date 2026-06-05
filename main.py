bd3da7858cd9cd50225c65b46800a49b8fdae3fb
Tanish bhardwaj <151875069+Stentwebber@users.noreply.github.com>
2026-06-06 00:35:35 +0200

    creating auth routes

--- main.py
+++ main.py
@@ -20,3 +20,42 @@ def load_user(user_id):
 
 with app.app_context():
     db.create_all()
+
+# ---------- AUTH ROUTES ----------
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
+
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created! Please login.")
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+
+        if user and check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash("Invalid credentials")
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
